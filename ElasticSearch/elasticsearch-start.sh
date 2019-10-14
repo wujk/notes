f02219976c77d912db6e1822ba9ad8021341a0ae
kevin <kevin@careerintlinc.com>
2019-10-14 15:15:06 +0800

    es集群配置

---
+++ ElasticSearch/elasticsearch-start.sh
@@ -0,0 +1,7 @@
+cd /usr/local/kevin/elasticsearch-cluster/9200
+./bin/elasticsearch -d
+cd /usr/local/kevin/elasticsearch-cluster/9201
+./bin/elasticsearch -d
+cd /usr/local/kevin/elasticsearch-cluster/9202
+./bin/elasticsearch -d
+
